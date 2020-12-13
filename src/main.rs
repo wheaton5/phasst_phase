7cea3863fa16d05d60f943a671148c04f75fd02e
wheaton5 <whheaton@gmail.com>
2020-12-13 14:52:52 -0600

    gather hic molecules

--- src/main.rs
+++ src/main.rs
@@ -1,134 +1,155 @@
 #[macro_use]
 extern crate clap;
-extern crate fnv;
 extern crate hashbrown;
-extern crate flate2;
-extern crate itertools;
 extern crate rayon;
-extern crate byteorder;
-extern crate disjoint_set;
+extern crate phasst_lib;
+extern crate rand;
+
+use phasst_lib::{Kmers, load_molecule_kmers, load_assembly_kmers, Variants, Molecules, Assembly};
 use rayon::prelude::*;
-use flate2::read::GzDecoder;
-use std::io::BufReader;
-use std::io::BufRead;
-use std::io::Read;
-use itertools::izip;
-use byteorder::{WriteBytesExt, LittleEndian};
 
-use std::fs::File;
-use std::io::{BufWriter, Write};
+use rand::Rng;
+use rand::rngs::StdRng;
+use rand::SeedableRng;
 
-use hashbrown::{HashMap, HashSet};
-use std::str;
+use hashbrown::{HashMap};
 
 use clap::{App};
 
 fn main() {
-    println!("Welcome to Phuzzy Phazer!");
+    println!("Welcome to phasst phase!");
     let params = load_params();
-    let kmers = load_kmers(&params.het_kmers);
+    let kmers = Kmers::load_kmers(&params.het_kmers);
+    let (variants, molecules) = load_molecule_kmers(&params.txg_mols, &params.hic_mols, &params.longread_mols, &kmers);
+    let assembly = load_assembly_kmers(&params.assembly_kmers, &kmers);
 
+    phasst_phase_main(&params, &variants, &molecules);
+    let variant_contig_order: HashMap<i32, (i32, usize)> = good_assembly_loci(&assembly);
+    let hic_links: HashMap<i32, Vec<HIC>> = gather_hic_links(&molecules, &variant_contig_order);
 }
 
-#[derive(PartialEq)]
-enum KmerType {
-    PairedHet,
-    UnpairedHet,
-    Homozygous,
-}
+fn good_assembly_loci(assembly: &Assembly) ->  HashMap<i32, (i32, usize)> { // returning a map from kmer id to contig and position
+ 
+    let mut variant_contig_order: HashMap<i32, (i32, usize)> = HashMap::new();
 
-struct Kmers {
-    kmers: HashMap<i32, String>,
-    kmer_counts: HashMap<i32, i32>,
-    kmer_type: HashMap<i32, KmerType>,
-}
+    let mut contig_positions: HashMap<i32, Vec<(usize, i32, i32)>> = HashMap::new();
+    for (kmer, (contig, num, _order, position)) in assembly.variants.iter() {
+        if assembly.variants.contains_key(&Kmers::pair(*kmer)) { continue; } // we see both ref and alt in assembly, skip
+        if *num > 1 { continue; } // we see this kmer multiple times in the assembly, skip
+        let positions = contig_positions.entry(*contig).or_insert(Vec::new());
+        positions.push((*position, *kmer, *contig));
+    }
 
-fn load_kmers(kmerfile: &String) -> Kmers {
-    let mut kmers: HashMap<i32, String> = HashMap::new();
-    let mut kmer_id: i32 = 1; // no 0 kmer id as we are using sign for which of the pair
-    let mut kmer_type: HashMap<i32, KmerType> = HashMap::new();
-    let mut kmer_counts: HashMap<i32, i32> = HashMap::new();
-    let reader = File::open(kmerfile).expect("cannot open kmer file");
-    let mut reader = BufReader::new(reader);
-    let mut buf1 = vec![]; let mut buf2 = vec![];
-    let mut buf3 = vec![]; let mut buf4 = vec![];
-    loop {
-        buf1.clear(); buf2.clear(); buf3.clear(); buf4.clear();
-
-        let bytes1 = reader.read_until(b'\t', &mut buf1).expect("cannot read file");
-        if bytes1 == 0 { break; } 
-   
-        let bytes2 = reader.read_until(b'\t', &mut buf2).expect("cannot read file");
-        if bytes2 == 0 { break; } 
-
-        let bytes3 = reader.read_until(b'\t', &mut buf3).expect("cannot read file");
-        if bytes3 == 0 { break; } 
-
-        let bytes4 = reader.read_until(b'\n', &mut buf4).expect("cannot read file");
-        if bytes4 == 0 { break; } 
-
-        let switchme = std::str::from_utf8(&buf3).unwrap();
-        match switchme {
-            "HET\t" => {
-                kmer_type.insert(kmer_id, KmerType::UnpairedHet);
-                kmer_type.insert(-kmer_id, KmerType::UnpairedHet);
-                kmers.insert(kmer_id, std::str::from_utf8(&buf1[0..(bytes1-1)]).unwrap().to_string());
-                //println!("")
-            },
-            "HOM\t" => {
-                kmer_type.insert(kmer_id, KmerType::Homozygous);
-                kmer_type.insert(-kmer_id, KmerType::Homozygous);
-            },
-            _ => {
-                kmer_type.insert(kmer_id, KmerType::PairedHet);
-                kmer_type.insert(-kmer_id, KmerType::PairedHet);
-                kmer_type.insert(kmer_id+1, KmerType::PairedHet);
-                kmer_type.insert(-(kmer_id+1), KmerType::PairedHet);
-                kmers.insert(kmer_id, std::str::from_utf8(&buf1[0..(bytes1-1)]).unwrap().to_string());
-                kmers.insert(-kmer_id, std::str::from_utf8(&buf1[0..(bytes1-1)]).unwrap().to_string());
-                let count = std::str::from_utf8(&buf2[0..(bytes2-1)]).unwrap().to_string().parse::<i32>().unwrap();
-                kmer_counts.insert(kmer_id, count);
-                kmer_counts.insert(-kmer_id, count);
-                kmers.insert(kmer_id+1, std::str::from_utf8(&buf3[0..(bytes3-1)]).unwrap().to_string());
-                kmers.insert(-(kmer_id+1), std::str::from_utf8(&buf3[0..(bytes3-1)]).unwrap().to_string());
-                let count = std::str::from_utf8(&buf4[0..(bytes4-1)]).unwrap().to_string().parse::<i32>().unwrap();
-                kmer_counts.insert(kmer_id+1, count);
-                kmer_counts.insert(-(kmer_id+1), count);
-            },
+    for (_contig, positions) in contig_positions {
+        let mut poses: Vec<(usize, i32, i32)> = Vec::new();
+        for (pos, kmer, contig) in positions {
+            poses.push((pos, kmer, contig));
         }
-        kmer_id += 2;
+        poses.sort();
+        for (index, (_position, kmer, contig)) in poses.iter().enumerate() {
+            variant_contig_order.insert(*kmer, (*contig, index));
+            variant_contig_order.insert(Kmers::pair(*kmer), (*contig, index));
+        }
     }
-    //(kmers, kmer_counts)
-    Kmers{
-        kmers: kmers,
-        kmer_counts: kmer_counts,
-        kmer_type: kmer_type,
+
+    return variant_contig_order;
+}
+
+struct HIC {
+    loci: Vec<usize>,
+    alleles: Vec<f32>,
+}
+
+fn gather_hic_links(molecules: &Molecules, variant_contig_order: &HashMap<i32, (i32, usize)>) -> HashMap<i32, Vec<HIC>> {
+    let mut hic_mols: HashMap<i32, Vec<HIC>> = HashMap::new();
+
+    let mut contig_mols: HashMap<i32, HashMap<i32, Vec<(i32, usize)>>> = HashMap::new();
+    for mol in molecules.get_hic_molecules() {
+        for var in molecules.get_hic_variants(*mol) {
+            if let Some((contig, order)) = variant_contig_order.get(&var.abs()) {
+                let mols = contig_mols.entry(*contig).or_insert(HashMap::new());
+                let vars = mols.entry(*mol).or_insert(Vec::new());
+                vars.push((var.abs(), *order));
+            }
+        }
     }
+    for (contig, mols) in contig_mols.iter() {
+        let mut hic: Vec<HIC> = Vec::new();
+        for (mol, vars) in mols.iter() {
+            if vars.len() > 1 {
+                let mut loci: Vec<usize> = Vec::new();
+                let mut alleles: Vec<f32> = Vec::new();
+                for (var, index) in vars {
+                    loci.push(*index);
+                    if var % 2 == 0 {
+                        alleles.push(0.0);
+                    } else {
+                        alleles.push(1.0);
+                    }
+                }
+                hic.push(HIC{alleles: alleles, loci: loci});
+            }
+        }
+        hic_mols.insert(*contig, hic);
+    }
+
+    hic_mols
 }
 
 
-fn get_reader(filename: String) -> BufReader<Box<dyn Read>> {
-    let filetype: Vec<&str> = filename.split(".").collect();
-    let filetype = filetype[filetype.len()-1];
-    let file = match File::open(filename.clone()) {
-        Ok(file) => file,
-        Err(error) => panic!("There was a problem opening the file: {:?}", error),
-    };
-    let reader: Box<dyn Read> = match filetype {
-        "gz" => Box::new(GzDecoder::new(file)),
-        _ => Box::new(file),
-    };
-    BufReader::new(reader)
+struct ThreadData {
+    best_total_log_probability: HashMap<u32, f32>, // best log prob for each contig
+    rng: StdRng,
+    solves_per_thread: usize,
+    thread_num: usize,
 }
+
+impl ThreadData {
+    fn from_seed(seed: [u8; 32], solves_per_thread: usize, thread_num: usize) -> ThreadData {
+        ThreadData {
+            best_total_log_probability: HashMap::new(),
+            rng: SeedableRng::from_seed(seed),
+            solves_per_thread: solves_per_thread,
+            thread_num: thread_num,
+        }
+    }
+}
+
+fn phasst_phase_main(params: &Params, variants: &Variants, molecules: &Molecules) {
+    let seed = [params.seed; 32];
+    let mut rng: StdRng = SeedableRng::from_seed(seed);
+    let mut threads: Vec<ThreadData> = Vec::new();
+    let solves_per_thread = ((params.restarts as f32)/(params.threads as f32)).ceil() as usize;
+    for i in 0..params.threads {
+        threads.push(ThreadData::from_seed(new_seed(&mut rng), solves_per_thread, i));
+    }
+    threads.par_iter_mut().for_each(|thread_data| {
+
+    });
+
+}
+
+fn new_seed(rng: &mut StdRng) -> [u8; 32] {
+    let mut seed = [0; 32];
+    for i in 0..32 {
+        seed[i] = rng.gen::<u8>();
+    }
+    seed
+}
+
 
 #[derive(Clone)]
 struct Params {
     het_kmers: String,
-    txg_mols: Vec<String>,
-    hic_mols: Vec<String>,
+    txg_mols: Option<Vec<String>>,
+    hic_mols: Option<Vec<String>>,
+    longread_mols: Option<Vec<String>>,
     output: String,
     assembly_kmers: String,
     assembly_fasta: String,
+    threads: usize,
+    seed: u8,
+    restarts: u32,
 }
 
 fn load_params() -> Params {
@@ -136,18 +157,51 @@ fn load_params() -> Params {
     let params = App::from_yaml(yaml).get_matches();
     let het_kmers = params.value_of("het_kmers").unwrap();
     let output = params.value_of("output").unwrap();
-    let txg_tmp = match params.values_of("txg_mols") {
-        Some(x) => x.collect(),
-        None => Vec::new(),
+    let txg_tmp: Option<Vec<&str>> = match params.values_of("linked_read_mols") {
+        Some(x) => Some(x.collect()),
+        None => None,
     };
-    let mut txg_mols: Vec<String> = Vec::new();
-    for x in txg_tmp { txg_mols.push(x.to_string()); }
-    let hic_tmp =  match params.values_of("hic_mols") {
-        Some(x) => x.collect(),
-        None => Vec::new(),
+    let mut txg_mols: Option<Vec<String>> = None;
+    if let Some(mols) = txg_tmp {
+        let mut tmp = Vec::new();
+        for mol in mols {
+            tmp.push(mol.to_string());
+        }
+        txg_mols = Some(tmp);
+    }
+    
+    
+    let hic_tmp: Option<Vec<&str>> =  match params.values_of("hic_mols") {
+        Some(x) => Some(x.collect()),
+        None => None,
     };
-    let mut hic_mols: Vec<String> = Vec::new();
-    for x in hic_tmp { hic_mols.push(x.to_string()); }
+    let mut hic_mols = None;
+    if let Some(mols) = hic_tmp {
+        let mut tmp: Vec<String> = Vec::new();
+        for x in mols { tmp.push(x.to_string()); }
+        hic_mols = Some(tmp);
+    }
+
+    let long_tmp: Option<Vec<&str>> =  match params.values_of("longread_mols") {
+        Some(x) => Some(x.collect()),
+        None => None,
+    };
+    let mut long_mols = None;
+    if let Some(mols) = long_tmp {
+        let mut tmp: Vec<String> = Vec::new();
+        for x in mols { tmp.push(x.to_string()); }
+        long_mols = Some(tmp);
+    }
+
+    let threads = params.value_of("threads").unwrap_or("1");
+    let threads = threads.to_string().parse::<usize>().unwrap();
+
+    let seed = params.value_of("seed").unwrap_or("4");
+    let seed = seed.to_string().parse::<u8>().unwrap();
+
+    let restarts = params.value_of("restarts").unwrap_or("10");
+    let restarts = restarts.to_string().parse::<u32>().unwrap();
+    
     let assembly_kmers = params.value_of("assembly_kmers").unwrap();
     let assembly_fasta = params.value_of("assembly_fasta").unwrap();
     Params{
@@ -155,8 +209,12 @@ fn load_params() -> Params {
         output: output.to_string(),
         txg_mols: txg_mols,
         hic_mols: hic_mols,
+        longread_mols: long_mols,
         assembly_kmers: assembly_kmers.to_string(),
         assembly_fasta: assembly_fasta.to_string(),
+        threads: threads,
+        seed: seed,
+        restarts: restarts,
     }
 }
 

4c8dbd3053969ba36f0cbfbd0d174b06445629f5
wheaton5 <whheaton@gmail.com>
2020-12-23 14:49:37 -0600

    testing update on likelihoods not posteriors

--- src/main.rs
+++ src/main.rs
@@ -409,8 +409,11 @@ fn expectation_maximization(loci: usize, mut cluster_centers: Vec<Vec<f32>>, hic
             hic_likelihoods.push(read_likelihood);
             log_likelihood += read_likelihood;
             let probabilities = normalize_in_log(&log_likelihoods);
-            
-            update_sums_denoms(&mut sums, &mut denoms, hic_read, &probabilities);
+            let mut likelihoods = Vec::new();
+            for log_like in log_likelihoods.iter() {
+                likelihoods.push(log_like.exp());
+            }
+            update_sums_denoms(&mut sums, &mut denoms, hic_read, &likelihoods); // &probabilities); // TESTING
             hic_probabilities.push(probabilities);
             final_log_probabilities[readdex] = log_likelihoods;
         }
